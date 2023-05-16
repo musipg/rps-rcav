86ed29589b494e937449bde8a08f061b7d9a3130
Carlos Peniche <cpeniche@chicagobooth.edu>
2023-05-16 04:51:11 +0000

    Rock paper works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    #
+    
+    #redirect_to("https://www.wikipedia.org")
+    #render({ :plain => rand(100)})
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
 end
